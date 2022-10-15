c528e8adb6c50cbfc1b0b30485a9192ea6a0adaa
Khoa Nguyen <96245593+knguyen382@users.noreply.github.com>
2022-10-15 08:05:16 -0400

    Update bottle.ino

--- bottle.ino
+++ bottle.ino
@@ -1,8 +1,14 @@
+#include <HCSR04.h>
+
+HCSR04 hc(1, 2); //initialisation class HCSR04 (trig pin , echo pin)
+
 void setup()
 {
-    
+    Serial.begin(9600);
 }
+
 void loop()
 {
-    
+    Serial.println(hc.dist()); 
+    delay(60);                 
 }
