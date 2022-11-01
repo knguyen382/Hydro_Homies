e606651252b78bc678d87661c16e128227f32e0d
khoa.nguyen <khoatqnguyen@gmail.com>
2022-11-01 18:41:12 -0400

    wifi

--- bottle.ino
+++ bottle.ino
@@ -1,7 +1,5 @@
 #include <HCSR04.h>
-#include <ArduinoBLE.h>
-
-
+#include <WiFi.h>
 
 #define TRIGGER_PIN  0  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     1  // Arduino pin tied to echo pin on the ultrasonic sensor.
@@ -13,16 +11,55 @@ float radius = 4.5;
 float height_water;
 float height_sensor;
 
+// Replace with your network credentials
+
+const char* ssid = "abc";
+const char* password = "6785702328";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Assign output variables to GPIO pins
+const int output26 = 34;
+const int output27 = 27;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
 HCSR04 sonar(TRIGGER_PIN, ECHO_PIN);
-BLEService hydro_homies("19B10000-E8F2-537E-4F6C-D104768A1214");
 
 //NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
-  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
-  BLE.setLocalName("Hydro");
-  BLE.setAdvertisedService(hydro_homies);
+  Serial.begin(115200);
+  // Initialize the output variables as outputs
+  pinMode(output26, OUTPUT);
+  pinMode(output27, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output26, LOW);
+  digitalWrite(output27, LOW);
 
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
 }
 
 void loop() {
@@ -42,7 +79,7 @@ void loop() {
   Serial.print(3.14 * radius * radius * height_water);
   Serial.println("mL");*/
   volume_calculation();
-  Serial.println(Volume);
+  web_app();
   
 }
 
@@ -64,3 +101,72 @@ void volume_calculation()
   delay(10);
 }
 
+
+void web_app()
+{
+  WiFiClient client = server.available();   // Listen for incoming clients
+  if (client) 
+  {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>Hydro Homies</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            client.println("<p>Water level: </p>");
+            client.print(Volume);
+            client.print(" oz. ");
+            client.println("</body></html>");
+
+
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
