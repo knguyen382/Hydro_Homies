b1dddf32ec0bf76300ff2c3ef2c099cad8b5f0a9
khoa.nguyen <khoatqnguyen@gmail.com>
2022-11-03 16:44:09 -0400

    web app ready

--- bottle.ino
+++ bottle.ino
@@ -1,43 +1,34 @@
 #include <HCSR04.h>
 #include <WiFi.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
 
-#define TRIGGER_PIN  0  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     1  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define TRIGGER_PIN  21  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     19  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-long unsigned int Volume;
-//float PI = 3.1415;
-float radius = 4.5;
-float height_water;
-float height_sensor;
-float volume;
+
+bool gender_male = false;
+float water_consumed;
+float water_remaining;
+float daily_water_intake;
+
+float duration[10], distance;
+float duration_sum = 0;
+float duration_avg;
+int i;
+
 // Replace with your network credentials
 
-const char* ssid = "TheCoffeeByHand";
-const char* password = "7702328850";
-const char* desired_water_intake = "input";
+const char* ssid = "abc";
+const char* password = "6785702328";
 
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html><head>
-  <title>Desired water Intake: </title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head><body>
-  <form action="/get">
-    input1: <input type="text" name="input">
-    <input type="submit" value="Submit">
-  </form><br>
-  </body></html>)rawliteral";
+const char* gender;
 
 // Set web server port number to 80
-AsyncWebServer server(80);
+WiFiServer server(80);
 
 // Variable to store the HTTP request
 String header;
 
-// Assign output variables to GPIO pins
-const int output26 = 34;
-const int output27 = 27;
 
 // Current time
 unsigned long currentTime = millis();
@@ -48,19 +39,11 @@ const long timeoutTime = 2000;
 
 HCSR04 sonar(TRIGGER_PIN, ECHO_PIN);
 
+//NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
   Serial.begin(115200);
-  // Initialize the output variables as outputs
-  pinMode(output26, OUTPUT);
-  pinMode(output27, OUTPUT);
-
-  // Set outputs to LOW
-  digitalWrite(output26, LOW);
-  digitalWrite(output27, LOW);
-  web_app();
-  // Connect to Wi-Fi network with SSID and password
-  /*Serial.print("Connecting to ");
+  Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -72,94 +55,122 @@ void setup() {
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  server.begin();*/
-  //web_app();
+  server.begin();
 }
 
 void loop() {
-  //delay(200);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(ECHO_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
 
-  /*height_sensor = sonar.dist();
-  Serial.print("Height of sensor = ");
-  Serial.print(height_sensor); // Send ping, get distance in cm and print result (0 = outside set distance range)
-  Serial.println("cm");
-
-  height_water = (9.5) - (height_sensor);
-  Serial.print("Height of water = ");
-  Serial.print(height_water); // Send ping, get distance in cm and print result (0 = outside set distance range)
-  Serial.println("cm");
-
-  Serial.print("Volume: ");
-  Serial.print(3.14 * radius * radius * height_water);
-  Serial.println("mL");*/
-  volume_calculation();
-  Serial.println(Volume);
-  
-  
-}
-
-void volume_calculation()
-{
-  float water_level = 0;// store level in every step
-  int read_value = 0; //read sensor reading in cm
-  float average_water_level;
-  int water_amount_in_ounce;
-  for(int i=0; i<5; i++)
-  { //take five reading
-      read_value = sonar.dist();
-      water_level = water_level + read_value;
-        
+  if (i < sizeof(duration)/sizeof(float)){
+    duration [i] = pulseIn(ECHO_PIN, HIGH);
+    Serial.print("Duration: ");
+    Serial.println(duration [i]);
+    i++;
   }
-  average_water_level = 9.5 - water_level/5; //find average from five reading, 17 = botole height
-  water_amount_in_ounce = int(3.14 * radius * radius * water_level);
-  Volume = water_amount_in_ounce;
-  delay(10);
+  else{
+    for (int j = 0; j < sizeof(duration)/sizeof(float); j++){
+      duration_sum = duration_sum + duration[j];      
+    }
+    duration_avg = duration_sum/(sizeof(duration)/sizeof(float));
+    distance = (duration_avg*.0343)/2;
+    i = 0;
+    Serial.println("");
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    duration_sum = 0;
+  }
+  delay(500);
+
+  web_app();
+ 
 }
 
 
 void web_app()
 {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("WiFi Failed!");
-    return;
+  WiFiClient client = server.available();   // Listen for incoming clients
+  if (client) 
+  {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            
+            // buttons
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>Hydro Homies</h1>");
+            
+            // Select your gender
+            client.println("<p>Select your gender: </p>");
+            client.println("<p><a href=\"/M\"><button class=\"button\">MALE</button></a></p>");
+            client.print("<p><a href=\"/F\"><button class=\"button\">FEMALE</button></a></p>");
+
+            client.println("<p>Your recommended daily water intake is: </p>");
+            client.print(daily_water_intake);
+            client.print(" oz. ");
+
+            client.println("<p>Water consumed: </p>");
+            client.print(distance);
+            client.print(" oz. ");
+
+            client.println("<p>Water remaining: </p>");
+            client.print(water_remaining);
+            client.print(" oz. ");
+            client.println("</body></html>");
+       
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+        if (currentLine.endsWith("GET /M")) {
+          daily_water_intake = 104;             
+        }
+        if (currentLine.endsWith("GET /F")) {
+          daily_water_intake = 72;               
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
-  Serial.println();
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-
-  // Send web page with input fields to client
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html);
-  });
-
-  // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
-  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    String inputMessage;
-    String inputParam;
-    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
-    if (request->hasParam(desired_water_intake)) {
-      inputMessage = request->getParam(desired_water_intake)->value();
-      inputParam = desired_water_intake;
-    }
-    else {
-      inputMessage = "No message sent";
-      inputParam = "none";
-    }
-    Volume = atoi( inputMessage.c_str() );
-    Serial.println(inputMessage);
-    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
-                                     + inputParam + ") with value: " + inputMessage +
-                                     "<br><a href=\"/\">Return to Home Page</a>");
-  });
-  
-
-  server.onNotFound(notFound);
-  server.begin();
-  
-}
-
-void notFound(AsyncWebServerRequest *request) {
-  request->send(404, "text/plain", "Not found");
 }
